1d8957e2607f461f610029a4f722b1ea5d7cd9a8
Andre Lima <hello@andrelima.space>
2021-03-28 11:12:04 -0300

    Created types that we should expect from the API

---
+++ src/types/api.ts
@@ -0,0 +1,16 @@
+export type Author = {
+    name: string;
+    avatar: string;
+}
+
+export type Message = {
+    createdAt: Date;
+    id: string;
+    author: Author;
+    body: string;
+}
+
+export type Channel = {
+    id: string;
+    label: string;
+}
